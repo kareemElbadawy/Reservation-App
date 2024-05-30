44772764c1bed75f54c895dc7c508f37d4b951f4
kareemelbadawy20 <staff6@net3000.ca>
2024-05-30 23:46:36 +0300

    Add project files.

---
+++ Extensions/RegisterExtensions.cs
@@ -0,0 +1,71 @@
+﻿using Reservation.Core.EFContext;
+using Reservation.Core.Repositories.Base;
+using Reservation.Core.Repositories.Interface;
+using Reservation.Core.Uow;
+using Reservation.Domain.Identity;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore.Internal;
+using Microsoft.EntityFrameworkCore;
+using Reservation.Core.Factory;
+using Reservation.Services;
+
+namespace Reservation_App.Extensions
+{
+	internal static class RegisterExtensions
+	{
+		internal static void AddDbContexts(this IServiceCollection services, IConfiguration configuration, IWebHostEnvironment environment)
+		{
+			var contextConnectionString = configuration.GetConnectionString("DefaultConnection");
+			services.AddDbContextPool<DatabaseContext>(x => x.UseSqlServer(contextConnectionString, o =>
+			{
+				o.EnableRetryOnFailure(3);
+			})
+				.EnableSensitiveDataLogging(environment.IsDevelopment())
+				.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking));
+		}
+
+		internal static void AddInjections(this IServiceCollection services)
+		{
+			services.AddScoped<IDatabaseContext, DatabaseContext>();
+			services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+			services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+			//services.AddTransient(typeof(IPlayerServices), typeof(PlayersServices));
+			//services.AddTransient(typeof(IPositionsServices), typeof(PositionsServices));
+			services.AddScoped<IContextFactory, ContextFactory>();
+			services.AddScoped<IUnitOfWork, UnitOfWork>();
+		}
+
+		internal static void AddIdentity(this IServiceCollection services)
+		{
+
+			services.AddIdentity<ApplicationUser, IdentityRole>()
+			.AddRoleManager<RoleManager<IdentityRole>>()
+			.AddDefaultTokenProviders()
+			.AddEntityFrameworkStores<DatabaseContext>();
+
+			services.AddIdentity<ApplicationUser, IdentityRole>()
+				.AddEntityFrameworkStores<DatabaseContext>()
+				.AddDefaultTokenProviders();
+
+			services.Configure<IdentityOptions>(options =>
+			{
+				// Password settings
+				options.Password.RequireDigit = false;
+				options.Password.RequiredLength = 1;
+				options.Password.RequireNonAlphanumeric = false;
+				options.Password.RequireUppercase = false;
+				options.Password.RequireLowercase = false;
+				options.Password.RequiredUniqueChars = 1;
+
+				// Lockout settings
+				options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(30);
+				options.Lockout.MaxFailedAccessAttempts = 5;
+				options.Lockout.AllowedForNewUsers = true;
+
+				// User settings
+				options.User.RequireUniqueEmail = false;
+			});
+		}
+	}
+
+}
---
+++ Startup.cs
@@ -0,0 +1,107 @@
+﻿using Reservation.Core;
+using Reservation_App.Extensions;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+
+namespace Reservation_App.Server
+{
+	public class Startup
+	{
+		private IWebHostEnvironment Environment { get; }
+		public Startup(IConfiguration configuration, IWebHostEnvironment env)
+		{
+			Configuration = configuration;
+
+			Environment = env;
+		}
+
+		public IConfiguration Configuration { get; }
+
+		// This method gets called by the runtime. Use this method to add services to the container.
+		public void ConfigureServices(IServiceCollection services)
+		{
+			services.AddDbContexts(Configuration, Environment);
+			services.AddInjections();
+			services.AddIdentity();
+			//services.AddAutoMapper(typeof(MappingProfile));
+
+			services.AddCors(options =>
+			{
+				options.AddDefaultPolicy(
+					builder =>
+					{
+						builder.WithOrigins("*", "http://localhost:4200")
+											.AllowAnyHeader()
+											.AllowAnyMethod();
+					});
+			});
+
+
+			services.AddControllers();
+
+			services.AddSwaggerGen(swagger =>
+			{
+				swagger.SwaggerDoc("v1", new OpenApiInfo { Title = "Hotel Reservation", Version = "v1" });
+
+				// To Enable authorization using Swagger (JWT)    
+				swagger.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+				{
+					Name = "Authorization",
+					Type = SecuritySchemeType.ApiKey,
+					Scheme = "Bearer",
+					BearerFormat = "JWT",
+					In = ParameterLocation.Header,
+					Description = "Enter 'Bearer' [space] and then your valid token in the text input below.\r\n\r\nExample: \"Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9\"",
+				});
+				swagger.AddSecurityRequirement(new OpenApiSecurityRequirement
+				{
+					{
+						  new OpenApiSecurityScheme
+							{
+								Reference = new OpenApiReference
+								{
+									Type = ReferenceType.SecurityScheme,
+									Id = "Bearer"
+								}
+							},
+							new string[] {}
+
+					}
+				});
+			});
+
+
+		}
+
+		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+		public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+		{
+			app.UseHttpsRedirection();
+			app.UseDefaultFiles();
+			app.UseStaticFiles();
+			app.UseRouting();
+			app.UseCors(builder =>
+			{
+				builder
+				.AllowAnyOrigin()
+				.AllowAnyMethod()
+				.AllowAnyHeader();
+			});
+
+			app.UseAuthentication();
+			app.UseAuthorization();
+			app.UseSwagger();
+			app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Luftborn v1"));
+
+			app.UseEndpoints(endpoints =>
+			{
+				endpoints.MapControllers();
+				endpoints.MapFallbackToFile("/index.html");
+			});
+
+		}
+	}
+
+}
